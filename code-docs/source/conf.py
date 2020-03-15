7b45ddd4a649dc5e43df88379927fff8183db82a
Shaden Smith <Shaden.Smith@microsoft.com>
2020-03-15 13:57:22 -0700

    GitHub integration.

--- code-docs/source/conf.py
+++ code-docs/source/conf.py
@@ -57,3 +57,12 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# GitHub integration
+html_context = {
+    "display_github": True,
+    "github_user": "microsoft",
+    "github_repo": "DeepSpeed",
+    "github_version": "master",
+    "conf_py_path": "/code-docs/",
+}
