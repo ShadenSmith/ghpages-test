27a2050227140d6c74229ef7b037c0554dd02d2a
Shaden Smith <Shaden.Smith@microsoft.com>
2020-03-15 14:11:13 -0700

    updating path for RTD

--- code-docs/source/conf.py
+++ code-docs/source/conf.py
@@ -12,7 +12,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../../deepspeed'))
+sys.path.insert(0, os.path.abspath('deepspeed'))
 
 
 # -- Project information -----------------------------------------------------
