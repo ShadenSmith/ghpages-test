cb455e46564fe25d8b25d69ed1c5f4a830bdcdef
Shaden Smith <Shaden.Smith@microsoft.com>
2020-03-15 20:48:37 -0700

    mock torch module

--- docs/code-docs/source/conf.py
+++ docs/code-docs/source/conf.py
@@ -21,8 +21,8 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['torch']
-sys.modules.update((mod_name, Mock) for mod_name in MOCK_MODULES)
+MOCK_MODULES = ['torch', 'torch.distributed']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 sys.path.insert(0, os.path.abspath('../../../'))
 
