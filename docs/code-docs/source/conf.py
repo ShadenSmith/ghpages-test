2f4a40f494adc926c14ffbfd43a2adfc75ead972
Shaden Smith <Shaden.Smith@microsoft.com>
2020-03-15 21:04:09 -0700

    mock torch module

--- docs/code-docs/source/conf.py
+++ docs/code-docs/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('../../../'))
 class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
-            return Mock()
+            return MagicMock()
 
 MOCK_MODULES = ['torch']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
